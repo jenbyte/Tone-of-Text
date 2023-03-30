7fbc14bac413c88e09879250c61d5a487fc35ed4
Jennifer Yiu <jen.yiu.90@gmail.com>
2023-03-29 17:13:58 -0700

    add fetch call for OpenAI API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [tweet, setTweet] = useState("");
+	const [tweet, setTweet] = useState("");
   const [sentiment, setSentiment] = useState(""); //"Negative" or "Positive"
+	const OPENAI_API_KEY = import.meta.env.VITE_API_KEY;
 
-  function callOpenAIAPI() {
-    console.log("Calling the OpenAI API")
+  async function callOpenAIAPI() {
+    console.log("Calling the OpenAI API");
+
+    const APIBody = {
+      "model": "text-davinci-003",
+      "prompt": "Is the sentiment of this tweet positive or negativ (but with an emoji)? " + tweet,
+      "temperature": 0,
+      "max_tokens": 60,
+      "top_p": 1.0,
+      "frequency_penalty": 0.0,
+      "presence_penalty": 0.0
+    };
+
+    await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {  
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + OPENAI_API_KEY
+      },
+      body: JSON.stringify(APIBody)
+    }).then((data) => {
+			return data.json();
+		}).then((data) => {
+			console.log(data)
+			setSentiment(data.choices[0].text.trim());
+		});
   }
 
   console.log({tweet})
